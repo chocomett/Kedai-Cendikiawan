521150e00310ed23525e1541b8e6d6bc9d1e6836
Mett <164408568+chocomett@users.noreply.github.com>
2025-02-25 00:47:25 +0700

    Feat(Navbar): styling navbar Sidebar Toggle

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// togglee
+const navbarNav = document.querySelector('.navbar-nav');
+
+// Ketika hamburger menu di Click
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+}
+
+// Click diluar sidebar untuk menghilangkan nav 
+const hamburger = document.querySelector('#hamburger-menu');
+document.addEventListener('click', function(e) {
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
