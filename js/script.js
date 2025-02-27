4f58b48a3fe5cfe04eade9f825aab80f317e9725
Mett <164408568+chocomett@users.noreply.github.com>
2025-02-27 23:26:50 +0700

    Style(Formatting): Prittier Html and Css

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
-// togglee
+// Memilih elemen dengan kelas 'navbar-nav'
 const navbarNav = document.querySelector('.navbar-nav');
 
-// Ketika hamburger menu di Click
+// Ketika hamburger menu di klik
 document.querySelector('#hamburger-menu').onclick = () => {
     navbarNav.classList.toggle('active');
 }
 
-// Click diluar sidebar untuk menghilangkan nav 
+// Memilih elemen dengan ID 'hamburger-menu'
 const hamburger = document.querySelector('#hamburger-menu');
+
+// Klik di luar sidebar untuk menghilangkan nav
 document.addEventListener('click', function(e) {
     if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
